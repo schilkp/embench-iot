24cc8a3f09260eaa39e15ef0de5022781162faea
Riccardo Tedeschi <riccardo.tedeschi6@unibo.it>
2025-05-02 17:50:25 +0200

    config: add optional profiling prints

--- config/riscv32/boards/cheshire/boardsupport.c
+++ config/riscv32/boards/cheshire/boardsupport.c
@@ -17,20 +17,37 @@
    #include "util.h"
    #include "printf.h"
 
+   uint64_t cycles_total, instr_total, cycle_count, instret_count;
+
    void
    initialise_board ()
    {
+    #if defined(PROFILE) && PROFILE == 1
     uint32_t rtc_freq = *reg32(&__base_regs, CHESHIRE_RTC_FREQ_REG_OFFSET);
     uint64_t reset_freq = clint_get_core_freq(rtc_freq, 2500);
     uart_init(&__base_uart, reset_freq, __BOOT_BAUDRATE);
+    #endif
    }
 
    void __attribute__ ((noinline)) __attribute__ ((externally_visible))
    start_trigger ()
    {
+     #if defined(PROFILE) && PROFILE == 1
+     __asm__ volatile("csrr %0, mcycle" : "=r"(cycle_count));
+     __asm__ volatile("csrr %0, minstret" : "=r"(instret_count));
+     cycles_total = -cycle_count;
+     instr_total = -instret_count;
+     #endif
    }
 
    void __attribute__ ((noinline)) __attribute__ ((externally_visible))
    stop_trigger ()
    {
+     #if defined(PROFILE) && PROFILE == 1
+     __asm__ volatile("csrr %0, mcycle" : "=r"(cycle_count));
+     __asm__ volatile("csrr %0, minstret" : "=r"(instret_count));
+     cycles_total += cycle_count;
+     instr_total += instret_count;
+     printf("tot_cycles = %d\n\rinstret = %d\n\r", cycles_total, instr_total);
+     #endif
    }
